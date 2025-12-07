74fb1154c22338de8fba2fe60596301c6d6cf7bd
Rahmade271 <rahmatangga73@gmail.com>
2025-12-07 11:59:00 +0700

    FE

--- src/mock/userPrefs.js
+++ src/mock/userPrefs.js
@@ -1,7 +1,7 @@
 //Menyimpan data preferensi pengguna tiruan (theme, fontSize) untuk menguji styling dinamis.
 
 export const MOCK_USER_PREFS = {
-    //layoutWidth: 'fullWidth', 
+    layoutWidth: 'fullWidth', 
     layoutWidth: 'mediumWidth', 
     fontStyle: 'default',
     theme: 'light', 
